b30e60775130ed9f3a2a21b0b5c3df3db3a53856
Elin Ronda <elro2506@student.miun.se>
2026-05-31 17:38:38 +0200

    Fixat about-sidan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Kurser } from './pages/kurser/kurser';
 import { Ramschema } from './pages/ramschema/ramschema';
+import { About } from './pages/about/about';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'kurser', component: Kurser },
-    { path: 'ramschema', component: Ramschema }
+    { path: 'ramschema', component: Ramschema },
+    { path: 'about', component: About }
 ];
