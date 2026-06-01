0eaa05e209c464894c45d09f86f99a01607fe46a
Elin Ronda <elro2506@student.miun.se>
2026-06-01 15:33:04 +0200

    Det är nu möjligt att se aktuella kurser i en tabell. Det går att sortera

--- src/app/pages/kurser/kurser.ts
+++ src/app/pages/kurser/kurser.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { Kurs } from '../../models/kurser';
+import { KursService } from '../../services/kurser';
 
 @Component({
   selector: 'app-kurser',
@@ -6,4 +8,56 @@ import { Component } from '@angular/core';
   templateUrl: './kurser.html',
   styleUrl: './kurser.css',
 })
-export class Kurser {}
+export class Kurser {
+kursschema = signal<Kurs[]>([]);
+error = signal<string | null>(null);
+sortColumn = signal<keyof Kurs>('courseCode');
+sortBy(column: keyof Kurs) {
+  this.sortColumn.set(column);
+}
+
+filterText = signal("");
+selectedSubject = signal("");
+subjects = computed(() => {
+const allSubjects = this.kursschema().map(kurs => kurs.subject);
+return [...new Set(allSubjects)].sort();
+});
+filterCourses = computed(() => {
+  const filter = this.filterText().trim().toLocaleLowerCase();
+  const subject = this.selectedSubject();
+  const column = this.sortColumn();
+
+  let courses = this.kursschema();
+
+  if(filter) {
+    courses = courses.filter(kurs => 
+    kurs.courseCode.toLowerCase().includes(filter) || 
+    kurs.courseName.toLowerCase().includes(filter)
+  );
+}
+
+if(subject) {
+  courses = courses.filter(kurs => kurs.subject === subject);
+}
+
+return [...courses].sort((a, b) => String(a[column]).localeCompare(String(b[column])));
+});
+
+kursService = inject(KursService);
+
+//Körs när användargränssnittet är renderat och klart
+ngOnInit() {
+  this.loadKursschema();
+}
+
+async loadKursschema() {
+  try {
+const response = await this.kursService.loadKurser();
+this.kursschema.set(response);
+console.table(this.kursschema());
+  } catch(error) {
+console.log(error);
+this.error.set("Kunde inte ladda data");
+  }
+}
+}
--- src/app/services/kurser.ts
+++ src/app/services/kurser.ts
@@ -1,6 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Kurs } from '../models/kurser';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Kurser {}
+export class KursService {
+  private url: string ="/miun_courses.json";
+
+  http = inject(HttpClient);
+
+  //Ladda kurser
+  async loadKurser(): Promise<Kurs[]> {
+    const kurser = this.http.get<Kurs[]>(this.url);
+    return await firstValueFrom(kurser);
+  }
+}
